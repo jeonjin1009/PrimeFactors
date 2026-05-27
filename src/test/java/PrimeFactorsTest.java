7ba40645d8b0a126d4e18f2e05e531ec29e46bd0
jin <jin1391.jeon@samsung.com>
2026-05-28 08:56:36 +0900

    first commit

---
+++ src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeFactorsTest {
+
+    @Test
+    void name() {
+        assertEquals(1,1);
+    }
+}
