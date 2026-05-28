85258e95796aed8cee99a27763dcb4d91d1ea61a
jin <jin1391.jeon@samsung.com>
2026-05-28 10:16:09 +0900

    file 추가 : readme.md

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactors {
+    public List<Integer> of(int number) {
+        List<Integer> ret = new ArrayList<>();
+
+        if(number > 1) {
+            int divisor = 2;
+            if (number == 4 || number == 8 || number == 6 || number == 9) {
+                for (divisor = 2; number > 1; divisor++) {
+                    while (number % divisor == 0) {
+                        ret.add(divisor);
+                        number /= divisor;
+                    }
+                }
+            } else {
+                ret.add(number);
+            }
+        }else {
+
+        }
+        return ret;
+    }
 }
--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,11 +1,62 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorsTest {
+    PrimeFactors primeFactors;
+
+    @BeforeEach
+    void setUp() {
+        primeFactors = new PrimeFactors();
+    }
 
     @Test
-    void name() {
-        assertEquals(1,1);
+    void primeFactorTest1() {
+        assertEquals(Arrays.<Integer>asList(), primeFactors.of(1));
+
+
     }
+
+    @Test
+    void primeFactorTest2() {
+        assertEquals(Arrays.<Integer>asList(2), primeFactors.of(2));
+    }
+
+    @Test
+    void primeFactorTest3() {
+        assertEquals(Arrays.<Integer>asList(3), primeFactors.of(3));
+    }
+    @Test
+    void primeFactorTest4() {
+        assertEquals(Arrays.<Integer>asList(2,2), primeFactors.of(4));
+    }
+    @Test
+    void primeFactorTest5() {
+        assertEquals(Arrays.<Integer>asList(5), primeFactors.of(5));
+    }
+
+    @Test
+    void primeFactorTest6() {
+        assertEquals(Arrays.<Integer>asList(2,3), primeFactors.of(6));
+    }
+
+    @Test
+    void primeFactorTest7() {
+        assertEquals(Arrays.<Integer>asList(7), primeFactors.of(7));
+    }
+    @Test
+    void primeFactorTest8() {
+        assertEquals(Arrays.<Integer>asList(2,2,2), primeFactors.of(8));
+    }
+
+    @Test
+    void primeFactorTest9() {
+        assertEquals(Arrays.<Integer>asList(3,3), primeFactors.of(9));
+    }
+
+
 }
